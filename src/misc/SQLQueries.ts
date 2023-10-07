89f6a8ea06ca8ae4c065a616e8c4528a0a6c28b9
Nicholas Lowe <nlowe@whereoware.com>
2023-10-07 10:25:20 -0400

    left join

--- src/misc/SQLQueries.ts
+++ src/misc/SQLQueries.ts
@@ -4,7 +4,7 @@ import { Utils } from './Utils'
 export const SQLQueries = {
   getTestByID: (testID: string) => `SELECT * from tests where id=${Utils.parameterize(testID)}`,
   getTestAndNicknameByID: (testID: string) =>
-    `SELECT tests.*,users.nick_name from tests INNER JOIN users ON tests.user_id = users.id where tests.id=${Utils.parameterize(testID)}`,
+    `SELECT tests.*,users.nick_name from tests LEFT JOIN users ON tests.user_id = users.id where tests.id=${Utils.parameterize(testID)}`,
   getQuestions: `SELECT * from questions order by "order" ASC`,
   getAllTestIDs: `SELECT id from tests`,
   getUserByID: (userID: string) => `SELECT * from users where id=${Utils.parameterize(userID)}`,
